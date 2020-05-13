125d5824247f6e4216dacf7e73333675d550ed47
huutaibui97 <huutaibui97@gmail.com>
2020-05-13 15:58:55 +0700

    update node mudules - yarn - test live server demo and package

---
+++ basic-default/demo.js
@@ -0,0 +1,26 @@
+// const people = [
+//     { name: "Wes", cool: true, country: "Canada" },
+//     { name: "Scott", cool: true, country: "Merica" },
+//     { name: "Snickers", cool: false, country: "Dog Country" }
+//   ];
+  
+//   people.forEach((person, index) => {
+//     console.table(person.name);
+//   });
+dogs.toString();                        // convert to string: results "Bulldog,Beagle,Labrador"
+dogs.join(" * ");                       // join: "Bulldog * Beagle * Labrador"
+dogs.pop();                             // remove last element
+dogs.push("Chihuahua");                 // add new element to the end
+dogs[dogs.length] = "Chihuahua";        // the same as push
+dogs.shift();                           // remove first element
+dogs.unshift("Chihuahua");              // add new element to the beginning
+delete dogs[0];                         // change element to undefined (not recommended)
+dogs.splice(2, 0, "Pug", "Boxer");      // add elements (where, how many to remove, element list)
+var animals = dogs.concat(cats,birds);  // join two arrays (dogs followed by cats and birds)
+dogs.slice(1,4);                        // elements from [1] to [4-1]
+dogs.sort();                            // sort string alphabetically
+dogs.reverse();                         // sort string in descending order
+x.sort(function(a, b){return a - b});   // numeric sort
+x.sort(function(a, b){return b - a});   // numeric descending sort
+highest = x[0];                         // first item in sorted array is the lowest (or highest) value
+x.sort(function(a, b){return 0.5 - Math.random()});     // random order sort
